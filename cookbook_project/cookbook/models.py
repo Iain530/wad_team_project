1da2cf41cdd5f7423ea5a365332de5be55ed9b00
Iain Cattermole <2189570c@student.gla.ac.uk>
2017-02-25 20:23:44 +0000

    added some context things to views

--- cookbook_project/cookbook/models.py
+++ cookbook_project/cookbook/models.py
@@ -2,6 +2,17 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+##class UserProfile(models.Model):
+##    user = models.OneToOneField(User)
+##
+##    saved_recipes = models.?????ToManyField(Recipe)
+##
+##    def __str__(self):
+##        return self.user
+##    def __unicode__(self):
+##        return self.user
+
+
 class Category(models.Model):
     # Primary key
     name = models.CharField(max_length=128, unique=True)
@@ -22,7 +33,7 @@ class Recipe(models.Model):
     views = models.PositiveIntegerField(default=0)
     rating = models.PositiveIntegerField(default=0)
     no_of_ratings = models.PositiveIntegerField(default=0)
-    upload_date = models.DateTimeField()
+    upload_date = models.DateTimeField(auto_now_add=True)
 	
     # Fields input by user
     ingredients = models.TextField()              # Change field type
@@ -30,6 +41,7 @@ class Recipe(models.Model):
     serves = models.PositiveSmallIntegerField()
     cooking_time = models.PositiveIntegerField()
     picture = models.ImageField(upload_to="recipe_images", blank="True")
+    
     is_vegetarian = models.BooleanField(default=False)
     is_vegan = models.BooleanField(default=False)
     is_gluten_free = models.BooleanField(default=False)
@@ -45,7 +57,7 @@ class Comment(models.Model):
     user = models.ForeignKey(User)
     recipe = models.ForeignKey(Recipe)
 	
-    date = models.DateTimeField()
+    upload_date = models.DateTimeField(auto_now_add=True)
     text = models.CharField(max_length=200)
 	
     def __str__(self):
--- cookbook_project/cookbook/views.py
+++ cookbook_project/cookbook/views.py
@@ -1,49 +1,107 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from cookbook.models import Category, Recipe, Comment
 
 #-HOME-SECTION----------------------------------------------------------
 
 def home(request):
-    # placeholder home view
-    return HttpResponse("This is the home")
+    context_dict = {}
+    best_rated = Recipes.objects.order_by('-rating')[:5]
+    new_recipes = Recipes.objects.order_by('-upload_date')[:5]
+    context_dict['best_rated'] = best_rated
+    context_dict['new_recipes'] = new_recipes
+    #when template is made
+    #response = render(request, 'cookbook/home.html', context_dict)
+    response = HttpResponse("This is the home")
+    return response
 
 #-USER-SECTION----------------------------------------------------------
 
 def signup(request):
+    #to do
     return HttpResponse("sign up here")
 
 def login(request):
+    #to do
     return HttpResponse("login")
 
+@login_required
 def myprofile(request):
-    return HttpResponse("my profileplaceholder")
+    context_dict = {}
+    
+    recent_comments = Comments.objects.filter(user=request.user).order_by(['-upload_date'])[:10]
+    recipes = Recipes.objects.filter(user=request.user).order_by(['-upload_date'])
 
+    context_dict['recent_comments'] = recent_comments
+    context_dict['recipes'] = recipes
+
+    return HttpResponse("my profile")
+
+@login_required
 def savedrecipes(request):
+    context_dict = {}
+    #to do
     return HttpResponse("saved recipes")
 
+@login_required
 def uploadrecipe(request):
+    #to do
     return HttpResponse("upload recipe")
 
 # view for a user profile
-def view_user(request):
+def view_user(request, user):
+    context_dict = {}
+
+    try:
+        user = User.objects.get(username=user)
+        recipes = Recipe.objects.filter(user=user)
+
+        context_dict['recipes'] = recipes
+
+    except User.DoesNotExist:
+        context_dict['recipes'] = None
+    
     return HttpResponse("view someone elses profile")
 
 # Actual recipe view
-def view_recipe(request):
-    return HttpResponse("view a recipe")
+def view_recipe(request, user, name):
+    context_dict = {}
 
+    try:
+        recipe = Recipe.objects.get(user=user, name=name)
+        context_dict['recipe'] = recipe
+
+    except Recipe.DoesNotExist:
+        context_dict['recipe'] = None
+    
+    return HttpResponse("view a recipe")
 
 
 #-CATEGORY-SECTION--(finding recipes)--------------------------------------
 
 def categories(request):
+    context_dict = {}
+    categories = Category.objects
+    cotext_dict['categories'] = categories
+    
     return HttpResponse("view all categories")
 
 # for all categhories
-def view_category(request):
+def view_category(request, category):
+    context_dict = {}
+
+    recipes = Recipe.objects.filter(category=category)
+    context_dict['recipes'] = recipes
+    
     return HttpResponse("specific category")
 
 def bestrated(request):
+    context_dict = {}
+    recipes = Recipes.objects.order_by('-rating')
+    context_dict['recipes'] = recipes
     return HttpResponse("best rated recipes here")
 
 # search and search results
