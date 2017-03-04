6c013f3fdbf44455cc4cb60f2530ce696437782f
hope.doolan@yahoo.com <hope.doolan@yahoo.com>
2017-03-04 17:45:28 +0000

    upload a recipe
    form created to allow user to upload a recipe

--- cookbook_project/cookbook/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from cookbook.models import Category, Recipe, Comment, Ingredient
-
-admin.site.register(Category)
-admin.site.register(Recipe)
-admin.site.register(Comment)
-admin.site.register(Ingredient)
-# Register your models here.
