931daa8516594dd72f56e09851d69bc1d69a7399
Iain Cattermole <2189570c@student.gla.ac.uk>
2017-03-13 15:25:33 +0000

    star rating on homepage and button styling

--- cookbook_project/static/js/cookbook-ajax.js
+++ cookbook_project/static/js/cookbook-ajax.js
@@ -1,6 +1,6 @@
 
 
-$('#save_button').click(function(){
+$('#save_button').click(function(event){
 	var user;
 	var slug;
 	slug = $(this).attr("data-slug");
@@ -8,8 +8,30 @@ $('#save_button').click(function(){
 	$.get('/cookbook/save_recipe/', {user: user, slug: slug}, function(data){
 		if (data === 'True') {
 			$('#save_button').html('Remove');
+		} else if (data == 'False') {
+			$('#save_button').html('Save');
 		} else {
-			$('#save_button').html('Save')
+			alert('Could not save/remove recipe');
 		}
 	});
 });
+
+// Posting comments (unfinished)
+
+// $('#post_comment').click(function() {
+	// var user;
+	// var slug;
+	// var comment;
+	
+	// slug = $(this).attr("data-slug");
+	// user = $(this).attr("data-user");
+	// comment = $(#'comment_text').val()
+	
+	// if (val != '') {
+		// $.get('/cookbook/post_comment', {user: user, slug: slug, text: comment}, function(data){
+			
+			
+		// });
+		
+	// }
+// });
--- cookbook_project/static/js/cookbook-jquery.js
+++ cookbook_project/static/js/cookbook-jquery.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
 	
+	// Display star rating on page
+	$(function() {
+		$('span.stars').stars();
+	});
+
+	
+	
+	$.fn.stars = function() {
+		return $(this).each(function() {
+			// Get the value
+			var val = parseFloat($(this).html());
+			// Make sure that the value is in 0 - 5 range, multiply to get width
+			var size = Math.max(0, (Math.min(5, val))) * 16;
+			// Create stars holder
+			var $span = $('<span />').width(size);
+			// Replace the numerical value with stars
+			$(this).html($span);
+		});
+	}
+	
+	
+	// From TWD (not used)
 	$("#about-btn").click( function(event) {
 		alert("You clicked the button using JQuery!");
 	});
