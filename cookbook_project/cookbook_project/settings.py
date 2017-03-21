a85e9c22673c1f9b0e74dcab915a319ad04e02a8
Peter Maitland <2200049m@student.gla.ac.uk>
2017-03-21 15:28:54 +0000

    added pythonanywhere to allowed hosts

--- cookbook_project/cookbook_project/settings.py
+++ cookbook_project/cookbook_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '7(olc*@nkxpj003ckd))6ahcy(#tfrta67=x0$e=bq6t&vmy*s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['2200049m.pythonanywhere.com']
 
 
 # Application definition
