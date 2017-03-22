ae24426971e4b234aa019264862e1746f8994855
Iain Cattermole <2189570c@student.gla.ac.uk>
2017-03-22 01:21:23 +0000

    changes to length of recipe name and desc to stop overlap

--- cookbook_project/cookbook/models.py
+++ cookbook_project/cookbook/models.py
@@ -63,8 +63,8 @@ class Recipe(models.Model):
 
 	
     # Fields input by user
-    MAX_NAME_LENGTH = 50
-    MAX_DESC_LENGTH = 100
+    MAX_NAME_LENGTH = 25
+    MAX_DESC_LENGTH = 65
     MAX_INS_LENGTH = 32767
     name = models.CharField(max_length=MAX_NAME_LENGTH)
     slug = models.SlugField() # name as slug
--- cookbook_project/cookbook_project/settings.py
+++ cookbook_project/cookbook_project/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-WHOOSH_INDEX = os.path.join(BASE_DIR, 'whoosh')
 
 
 
